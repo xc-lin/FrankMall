399932659be36ca22faa9ab76f9d384570a33d0b
lxc <48900984+xc-lin@users.noreply.github.com>
2022-07-15 10:37:29 +0800

    first commit

---
+++ frankmall_seckill/src/test/java/com/lxc/frankmall/seckill/Testd.java
@@ -0,0 +1,15 @@
+package com.lxc.frankmall.seckill;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Frank_lin
+ * @date 2022/7/11
+ */
+public class Testd {
+    @Test
+    public void a(){
+        Object k = true?new Integer(1):new Double(2.0);
+        System.out.println(k.getClass());
+    }
+}
